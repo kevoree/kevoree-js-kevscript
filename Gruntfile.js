36d764b8e227025ea5481018400427f56ee3e1a6
Maxime Tricoire <max.tricoire@gmail.com>
2015-03-26 16:37:53 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        browserify: {
+            browser: {
+                options: {
+                    browserifyOptions: {
+                        standalone: 'KevoreeKevscript'
+                    }
+                },
+                src: ['<%= pkg.main %>'],
+                dest: 'browser/<%= pkg.name %>.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('default', 'browserify');
+};
