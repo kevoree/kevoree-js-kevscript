25bf6fff362ac93685d9e1cf2f4b2ed2810a32d3
Maxime Tricoire <max.tricoire@gmail.com>
2016-12-19 14:34:11 +0100

    add some tests

--- test/server/spec/set.js
+++ test/server/spec/set.js
@@ -22,4 +22,41 @@ describe('KevScript - set', function () {
 			});
 		});
 	});
+
+	it('unknown.kevs', () => {
+		const script = readKevs('set/unknown.kevs');
+		return new Promise((resolve, reject) => {
+			this.kevs.parse(script, (err) => {
+				if (err) {
+					setTimeout(() => {
+						assert.equal(err.message, 'Attribute "unknown" does not exist in type "JavascriptNode". Set failed');
+						assert.deepEqual(err.pos, [35, 42]);
+						resolve();
+					});
+				} else {
+					reject(new Error('set/unknown.kevs should NOT be valid'));
+				}
+			});
+		});
+	});
+
+	it('frag-param.kevs', () => {
+		const script = readKevs('set/frag-param.kevs');
+		return new Promise((resolve, reject) => {
+			this.kevs.parse(script, (err, model) => {
+				if (err) {
+					reject(err);
+				} else {
+					setTimeout(() => {
+						const group = model.findGroupsByID('sync');
+						const fDic = group.findFragmentDictionaryByID('node');
+						const portParam = fDic.findValuesByID('port');
+
+						assert.equal(portParam.value, '9000');
+						resolve();
+					});
+				}
+			});
+		});
+	});
 });
