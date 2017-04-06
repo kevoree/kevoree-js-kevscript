fd3f19ca9a0a0a8b0ae7fd5e5c39d21563d00968
Maxime Tricoire <max.tricoire@gmail.com>
2017-04-06 16:24:21 +0200

    v4.1.6: validate param value directly from kevscript

--- test/server/spec/kevs/set.js
+++ test/server/spec/kevs/set.js
@@ -40,6 +40,57 @@ describe('KevScript - set', function () {
 		});
 	});
 
+	it('wrong-number.kevs', () => {
+		const script = readKevs('set/wrong-number.kevs');
+		return new Promise((resolve, reject) => {
+			this.kevs.parse(script, (err) => {
+				if (err) {
+					setTimeout(() => {
+						assert.equal(err.message, 'Attribute "period" in "/nodes[node]/components[ticker]" cannot be converted to a "long"');
+						assert.deepEqual(err.pos, [103, 108]);
+						resolve();
+					});
+				} else {
+					reject(new Error('set/wrong-number.kevs should NOT be valid'));
+				}
+			});
+		});
+	});
+
+	it('wrong-boolean.kevs', () => {
+		const script = readKevs('set/wrong-boolean.kevs');
+		return new Promise((resolve, reject) => {
+			this.kevs.parse(script, (err) => {
+				if (err) {
+					setTimeout(() => {
+						assert.equal(err.message, 'Attribute "random" in "/nodes[node]/components[ticker]" must have a value of "true" or "false"');
+						assert.deepEqual(err.pos, [103, 106]);
+						resolve();
+					});
+				} else {
+					reject(new Error('set/wrong-boolean.kevs should NOT be valid'));
+				}
+			});
+		});
+	});
+
+	it('non-optional.kevs', () => {
+		const script = readKevs('set/non-optional.kevs');
+		return new Promise((resolve, reject) => {
+			this.kevs.parse(script, (err) => {
+				if (err) {
+					setTimeout(() => {
+						assert.equal(err.message, 'Non-optional attribute "logLevel" in "/nodes[node]" must have a value');
+						assert.deepEqual(err.pos, [60, 62]);
+						resolve();
+					});
+				} else {
+					reject(new Error('set/non-optional.kevs should NOT be valid'));
+				}
+			});
+		});
+	});
+
 	it('frag-param.kevs', () => {
 		const script = readKevs('set/frag-param.kevs');
 		return new Promise((resolve, reject) => {
